f95d708982b30ffe48cb08a4a895760ffe52a566
GGRMR <wldmsdl9707@naver.com>
2019-04-04 19:50:33 +0900

    new file created.

---
+++ oddnum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 19:45:33 2019
+
+@author: samsung
+"""
+
+
+NumList = [i for i in range(1,101)]
+print(NumList)
+
+myList = [i for i in NumList if i%2 == 1]
+print(myList)
---
+++ tentoone.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+bubble_list
+
+@author: Park Jieun
+
+"""
+for i in range(10,0,-1):
+    print(i)
+    
+    
+    
+    
+
+
+
+    
+
+
